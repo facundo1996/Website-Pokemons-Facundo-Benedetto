6d280c999ac621440879fad8c7ec6135b9ccdcd9
facundo1996 <ffaacu1@gmail.com>
2020-03-04 00:04:13 -0300

    use el arrow function , traigo los pokemon una sola vez y el buscador lo hago dentro del array pokemon con un contador y entonces lo hago visible o no visible

--- week1-2/pokedex.js
+++ week1-2/pokedex.js
@@ -1,26 +1,37 @@
 
 let pokemon = [];
-let pokemon2 = [];
 
 window.addEventListener('load',function(){
-    listado('');
+    listado();
     document.getElementById('buscador').focus();
 })
 
 document.getElementById("buscadorimg").addEventListener("click", function(){
-    listado(document.getElementById("buscador").value.toLowerCase())
-
+    busqueda(document.getElementById("buscador").value.toLowerCase())
+    
 });
  document.getElementById("buscador").addEventListener("change", function(){
-    listado(document.getElementById("buscador").value.toLowerCase())
+    busqueda(document.getElementById("buscador").value.toLowerCase())
 
 }); 
 
+const busqueda = (buscar) => {
 
-function listado(buscar){
+    const divs = document.getElementsByClassName('alldiv'); 
+
+    for (var i = 0; i < divs.length; i++) {
+        divs[i].classList.add('novisible')
+        if(pokemon[i+1].name.substr(0,buscar.length) == buscar){
+            divs[i].classList.remove('novisible')
+        }
+    }
+}
+
+const listado = () => {
     
     document.getElementById("pokemonsdiv").innerHTML=""
-    for(let contador=1;contador<=150;contador++){
+
+    for(let contador=1;contador<=20;contador++){
         fetch("https://pokeapi.co/api/v2/pokemon/"+contador)
         .then((response) => {
             return response.json();
@@ -39,13 +50,7 @@ function listado(buscar){
                 type: typepok+typepok2
             };
             let xhtml = "" ;
-            xhtml+="<div class='alldiv"
-
-            if(buscar && namepok.substr(0,buscar.length)!=buscar) {
-                xhtml+=" novisible";
-            }
-
-            xhtml+="' id='pok"+contador+"'><img class='imgpokedex' src="+myJson.sprites.front_default+">"
+            xhtml+="<div class='alldiv' id='pok"+contador+"'><img class='imgpokedex' src="+myJson.sprites.front_default+">"
             xhtml+="<h3 id='h3name' class='h3name'>"+namepok.charAt(0).toUpperCase()+namepok.slice(1)+"</h3>"
             xhtml+="<h3 class='h3type'>Type: "+typepok+typepok2+"</h3>"
             xhtml+="<button class='addfav'>Add to favs</button></div>"
